e210dd9bd32b1b413566e36b867e8895c4e124aa
ykspub <78717511+ykspub@users.noreply.github.com>
2021-09-28 19:53:03 -0400

    fixed to work on new moomoo

--- invisfixer.user.js
+++ invisfixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Invis Fixer Module
 // @namespace    http://tampermonkey.net/
-// @version      0.4.20
+// @version      0.5.20
 // @description  Replacement module for invis fixer
 // @author       The Big Daddy
 // @match        *://*.moomoo.io/*
@@ -14,10 +14,10 @@
 if (!window.bundleHookModules) {
     window.bundleHookModules = [];
 }
-
+//function pi(e,t){var n=di[e.id];if(!n||t){var i=document.createElement("canvas");i.width=i.height=2.5*e.scale+lt+(c.list[e.id].spritePadding||0);
 window.bundleHookModules.push(function(bundlejs) {
     let fixerCode = 'if(typeof e.dir !== "undefined") {if (Math.abs(e.dir) > 50) {e.dir = 0;}}';
-    return bundlejs.replace('function ri(e,t){', 'function ri(e,t){' + fixerCode);
+    return bundlejs.replace('function pi(e,t){', 'function pi(e,t){' + fixerCode);
 });
 
 if (localStorage.getItem("INVISFIXERINSTALLED") != "true") {
