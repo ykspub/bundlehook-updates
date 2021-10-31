1dd69a7ebce0dec632c333bebf6673338a11651e
ykspub <78717511+ykspub@users.noreply.github.com>
2021-10-31 16:27:39 -0400

    Updated invis fixer

--- invisfixer.user.js
+++ invisfixer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Invis Fixer Module
 // @namespace    http://tampermonkey.net/
-// @version      0.5.20
+// @version      0.6.20
 // @description  Replacement module for invis fixer
 // @author       The Big Daddy
 // @match        *://*.moomoo.io/*
@@ -14,13 +14,14 @@
 if (!window.bundleHookModules) {
     window.bundleHookModules = [];
 }
-//function pi(e,t){var n=di[e.id];if(!n||t){var i=document.createElement("canvas");i.width=i.height=2.5*e.scale+lt+(c.list[e.id].spritePadding||0);
+
+//function ri(e,t){var n=ii[e.id];if(!n||t){var i=document.createElement("canvas");i.width=i.height=2.5*e.scale+5.5+(l.list[e.id].spritePadding||0);
 window.bundleHookModules.push(function(bundlejs) {
-    let fixerCode = 'if(typeof e.dir !== "undefined") {if (Math.abs(e.dir) > 50) {e.dir = 0;}}';
-    return bundlejs.replace('function pi(e,t){', 'function pi(e,t){' + fixerCode);
+    let fixerCode = 'if(typeof e.dir !== "undefined") {if (Math.abs(e.dir) > 50) {e.dir = 0;}} else {e.dir = 0;}';
+    return bundlejs.replace('var n=ii[e.id];if(!n||t){', fixerCode + 'var n=ii[e.id];if(!n||t){');
 });
 
-if (localStorage.getItem("INVISFIXERINSTALLED") != "true") {
-    alert("Invis fixer module installed!");
-    localStorage.setItem("INVISFIXERINSTALLED", "true");
+if (localStorage.getItem("INVISFIXERINSTALLED2") != "true") {
+    alert("Invis fixer module updated in light of new moomoo.io update");
+    localStorage.setItem("INVISFIXERINSTALLED2", "true");
 }
